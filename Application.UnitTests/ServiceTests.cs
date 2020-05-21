2754c9dfb89a12e889fa7c07686d5ada0d581805
Kirimi <kimandueric@gmail.com>
2020-05-21 17:07:13 +0300

    Update ServiceTests.cs

--- Application.UnitTests/ServiceTests.cs
+++ Application.UnitTests/ServiceTests.cs
@@ -5,7 +5,7 @@ namespace Application.UnitTests
     [TestClass]
     public class ServiceTests
     {
-        [TestMethod, Owner("IvanY")]
+        [TestMethod, Owner("Kirimi")]
         public void AddTest()
         {
             var service = new Service();
@@ -13,7 +13,7 @@ namespace Application.UnitTests
             Assert.AreEqual(3, service.Add(1, 2));
         }
 
-        [TestMethod, Owner("IvanY")]
+        [TestMethod, Owner("Kirimi")]
         public void Return3Test()
         {
             var service = new Service();
